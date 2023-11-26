b78128c21951ea23c981ce07ebed45ca60864c02
Elian <elianinf04@gmail.com>
2023-11-26 12:57:08 -0400

    Prueba Unitaria Exitosa

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const mensajeSaludo = require('./index');
 
 test('Prueba del mensaje de aludo', () => {
-    expect(mensajeSaludo()).toBe('Hello Elian Baez');
+    expect(mensajeSaludo()).toBe('Hello Elian Baez E.');
 });
